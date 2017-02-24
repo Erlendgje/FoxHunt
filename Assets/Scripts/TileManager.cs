dc215e23d8caaadcc57057c18de32e0a4de60d67
Erlend <erlendgje@gmail.com>
2017-02-24 16:37:36 +0100

    Lagt inn kommentarer i koden

--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -14,12 +14,14 @@ public class TileManager : MonoBehaviour {
     private float lat, lon;
 
     IEnumerator Start() {
+        //Check if gps is on
         while (!Input.location.isEnabledByUser) {
             yield return new WaitForSeconds(1f);
         }
 
         Input.location.Start(10f, 5f);
 
+        //Trying to connect to gps service
         int maxWait = 20;
         while(Input.location.status == LocationServiceStatus.Initializing && maxWait > 0) {
             yield return new WaitForSeconds(1f);
@@ -30,6 +32,7 @@ public class TileManager : MonoBehaviour {
             yield break;
         }
 
+        //Check if connection failed
         if(Input.location.status == LocationServiceStatus.Failed) {
             yield break;
         }
@@ -40,10 +43,12 @@ public class TileManager : MonoBehaviour {
 
         StartCoroutine(loadTiles(_settings.zoom));
 
+        //Check if gps is on
         while (Input.location.isEnabledByUser) {
             yield return new WaitForSeconds(1f);
         }
 
+        //Restart function if gps is turned off
         yield return StartCoroutine(Start ());
     }
 
@@ -51,13 +56,17 @@ public class TileManager : MonoBehaviour {
         lat = Input.location.lastData.latitude;
         lon = Input.location.lastData.longitude;
 
+        //URL to map
         string url = string.Format("https://api.mapbox.com/v4/mapbox.{5}/{0},{1},{2}/{3}x{3}@2x.png?access_token={4}", lon, lat, zoom, _settings.size, _settings.key, _settings.style);
 
         WWW www = new WWW(url);
+        //Loading url
         yield return www;
 
+        //Getting image
         texture = www.texture;
 
+        //Creating plane and applying map texture
         if(tile == null) {
             tile = GameObject.CreatePrimitive(PrimitiveType.Plane);
             tile.transform.localScale = Vector3.one * _settings.scale;
@@ -68,7 +77,7 @@ public class TileManager : MonoBehaviour {
         tile.GetComponent<Renderer>().material.mainTexture = texture;
 
         yield return new WaitForSeconds(1f);
-
+        //Updating map  
         yield return StartCoroutine(loadTiles(_settings.zoom));
     }
 
@@ -77,6 +86,8 @@ public class TileManager : MonoBehaviour {
         
     }
 
+
+    //Map settings
     [System.Serializable]
     public class Settings{
 
