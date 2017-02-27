13b790d0117bafe222ca7c373a63a6eae2dbab2f
Tuan Vo <dynamictash@gmail.com>
2017-02-27 18:46:31 +0100

    Spawns

---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+
+    public Transform SpawnTest;
+    public bool Spawned = false;
+    public int i;
+   
+    // Use this for initialization
+    void Start () {
+
+  
+        if (Spawned == false)
+        {
+            Spawned = true;
+            Instantiate(SpawnTest, new Vector3(0, 20, i), Quaternion.identity);
+                
+        }
+		
+	}
+
+
+    // Update is called once per frame
+    void Update () {
+
+
+
+        
+	}
+}
