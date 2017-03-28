270284e46af24e11ebab8c3d6327b5d7eac20f4d
Tuan Vo <dynamictash@gmail.com>
2017-03-28 18:23:49 +0200

    Lagt til high score, men utestet

--- Assets/Scripts/Coordinates/GOScript3.cs
+++ Assets/Scripts/Coordinates/GOScript3.cs
@@ -6,47 +6,56 @@ public class GOScript3 : MonoBehaviour {
 
     public decimal lt, ln;
     public int id;
-	private float speed = 50f;
-	public int score;
+    private float speed = 50f;
+    public int score;
+    public int highScore;
     public GameObject gameManager;
-	private GameManager3 gmScript;
-	private bool first;
+    private GameManager3 gmScript;
+    private bool first;
 
-	private void Awake() {
-		gameManager = GameObject.FindGameObjectWithTag("GameManager");
-		gmScript = gameManager.GetComponent<GameManager3>();
-		first = true;
-	}
+    private void Awake() {
+        gameManager = GameObject.FindGameObjectWithTag("GameManager");
+        gmScript = gameManager.GetComponent<GameManager3>();
+        first = true;
+    }
 
-	// Use this for initialization
-	void Start() {
-        
-        
+    // Use this for initialization
+    void Start() {
+        //Henter gamle high score, foreløpig 0 fra start
+        highScore = PlayerPrefs.GetInt("highscore", highScore);
     }
 
     // Update is called once per frame
     void Update() {
+        //Oppdaterer high scoren
+        if (score > highScore)
+        {
+            highScore = score;
+            PlayerPrefs.SetInt("highscore", highScore);
+        }
     }
 
     public void SetValues(decimal lt, decimal ln, int id, int score) {
 
-		this.id = id;
+        this.id = id;
 
-		if(lt < gmScript.northernmostPoint && lt > gmScript.southernmosttPoint && ln < gmScript.easternmostPoint && ln > gmScript.westernmostPoint) {
-			this.lt = lt;
-			this.ln = ln;
-			this.score = score;
+        if (lt < gmScript.northernmostPoint && lt > gmScript.southernmosttPoint && ln < gmScript.easternmostPoint && ln > gmScript.westernmostPoint) {
+            this.lt = lt;
+            this.ln = ln;
+            this.score = score;
 
 
-			if (first == true) {
-				transform.position = gmScript.MakeVector((float)ln, (float)lt);
-				first = false;
-			}
-			this.GetComponent<Renderer>().enabled = true;
-			transform.position = Vector3.MoveTowards(transform.position, gmScript.MakeVector((float)ln, (float)lt), speed * Time.deltaTime);
-		}
-		else {
-			this.GetComponent<Renderer>().enabled = false;
-		}
-	}
+            if (first == true) {
+                transform.position = gmScript.MakeVector((float)ln, (float)lt);
+                first = false;
+            }
+            this.GetComponent<Renderer>().enabled = true;
+            transform.position = Vector3.MoveTowards(transform.position, gmScript.MakeVector((float)ln, (float)lt), speed * Time.deltaTime);
+        }
+        else {
+            this.GetComponent<Renderer>().enabled = false;
+        }
+    }
+
+    
 }
